3a83d081a57e61cf7a0dbcd962f401195217217d
quocngo-git <ngoquoc.work@gmail.com>
2022-05-16 16:19:16 -0700

    body

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header.js';
+import Body from './Body.js';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Body />
       
     </div>
   );
---
+++ client/src/Body.js
@@ -0,0 +1,17 @@
+import './styles/Body.css';
+
+const Body = ()=>{
+    return(
+        <div className="body">
+            <div id="div-1">
+
+            </div>
+            <div id="div-2">
+
+            </div>
+        </div>
+        
+    )
+}
+
+export default Body;
