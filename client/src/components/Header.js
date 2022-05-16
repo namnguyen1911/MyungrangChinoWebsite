8216bae49572d451e85119f486d095afc84f092a
quocngo-git <ngoquoc.work@gmail.com>
2022-05-16 16:19:10 -0700

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,44 @@
 import '../styles/Header.css';
+import {LogoInstagram, LogoFacebook, LogoTwitter} from 'react-ionicons';
+
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="Header">
+            <div className="header-portion" id="header-1">
+                <div className="header-portion-1">
+                    Home
+                </div>
+                <div className="header-portion-1">
+                    About
+                </div>
+                <div className="header-portion-1">
+                    Feedback
+                </div>
+            </div>
+            <div className="header-portion" id="header-2">
+                {/* <img src="../../assets/logos/re/sign3-1.png" alt="LOGOS"></img> */}
+                
+            </div>
+            <div className="header-portion" id="header-3">
+                <div className="header-portion-3">
+                    <LogoFacebook 
+                        width="16px"
+                        heiht="auto"
+                    ></LogoFacebook>
+                </div>
+                <div className="header-portion-3">
+                    <LogoInstagram 
+                        width="16px"
+                        heiht="auto"
+                    ></LogoInstagram>
+                </div>
+                <div className="header-portion-3">
+                    <LogoTwitter 
+                        width="16px"
+                        heiht="auto"
+                    ></LogoTwitter>
+                </div>
+            </div>
         </div>
     )
 }
