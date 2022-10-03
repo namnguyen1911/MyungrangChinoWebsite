da688d13caa64d83609d03108eba579aac5a79b4
quocngo-git <ngoquoc.work@gmail.com>
2022-10-03 12:43:56 -0700

    pictures upload

--- client/src/Body.js
+++ client/src/Body.js
@@ -1,10 +1,11 @@
 import './styles/Body.css';
+import background1 from "./assets/6.Mozzarella & Sausage Hot Dog-1.png"
 
 const Body = ()=>{
     return(
         <div className="body">
             <div id="div-1">
-
+                <img src={background1} alt="Background 1"></img>
             </div>
             <div id="div-2">
                 <div className="div-2-header">
